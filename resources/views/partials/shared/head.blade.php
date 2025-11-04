14253beb7ab47c8453d87abc2dd45a07eb696043
My Dung <lemydung204@gmail.com>
2025-11-04 15:53:55 +0700

    cập nhật

--- resources/views/partials/shared/head.blade.php
+++ resources/views/partials/shared/head.blade.php
@@ -6,9 +6,7 @@
 <link rel="icon" href="{{ asset('image/favicon.png') }}" type="image/png">
 
 {{-- Fonts are self-hosted via resources/css/fonts.css and Vite build --}}
-@once
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-@endonce
+
 
 {{-- Layouts sẽ tự quyết định @vite entries (public vs admin) --}}
 @fluxAppearance
